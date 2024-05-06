c30143324828a68a78336ddcf190ea29eea4ba8b
Margie C. Henry <margieh@users.noreply.github.com>
2024-05-06 17:12:27 -0400

    format article layout

--- app.py
+++ app.py
@@ -1,16 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import feedparser
 from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 
+RSS_FEED = 'https://feeds.npr.org/1126/rss.xml'
+ARTICLES_PER_PAGE = 5
+
 @app.route('/')
 def home():
-    rss_feed = 'https://feeds.npr.org/1126/rss.xml'
-    feed = feedparser.parse(rss_feed)
+    page = request.args.get('page', default=1, type=int)
+    feed = feedparser.parse(RSS_FEED)
+    total_articles = len(feed.entries)
+    total_pages = (total_articles + ARTICLES_PER_PAGE - 1) // ARTICLES_PER_PAGE
+
+    start_index = (page - 1) * ARTICLES_PER_PAGE
+    end_index = min(start_index + ARTICLES_PER_PAGE, total_articles)
     articles = []
 
-    for entry in feed.entries:
+    for entry in feed.entries[start_index:end_index]:
         soup = BeautifulSoup(entry.content[0].value, 'html.parser')
         img_tag = soup.find('img')
         if img_tag:
@@ -27,7 +35,9 @@ def home():
         }
         articles.append(article)
 
-    return render_template('index.html', articles=articles)
+    return render_template('index.html', articles=articles, page=page, total_pages=total_pages)
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
