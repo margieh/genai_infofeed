19db45716d5b54e0e7d2b5a3542f39fe0eed1573
Margie C. Henry <margieh@users.noreply.github.com>
2024-05-06 16:48:48 -0400

    add rss feed

--- app.py
+++ app.py
@@ -1,25 +1,32 @@
 from flask import Flask, render_template
+import feedparser
+from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    # Sample articles
-    articles = [
-        {
-            'title': 'Article 1',
-            'content': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-            'source_link': 'https://example.com/article1',
-            'timestamp': '2024-05-06'
-        },
-        {
-            'title': 'Article 2',
-            'content': 'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-            'source_link': 'https://example.com/article2',
-            'timestamp': '2024-05-05'
-        },
-        # Add more sample articles as needed
-    ]
+    rss_feed = 'https://feeds.npr.org/1126/rss.xml'
+    feed = feedparser.parse(rss_feed)
+    articles = []
+
+    for entry in feed.entries:
+        soup = BeautifulSoup(entry.content[0].value, 'html.parser')
+        img_tag = soup.find('img')
+        if img_tag:
+            image_url = img_tag['src']
+        else:
+            image_url = None
+
+        article = {
+            'title': entry.title,
+            'description': entry.description,
+            'image_url': image_url,
+            'pubDate': entry.published,
+            'link': entry.link
+        }
+        articles.append(article)
+
     return render_template('index.html', articles=articles)
 
 if __name__ == '__main__':
